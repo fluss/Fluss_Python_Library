12a6f3e25923fd85150f9f9e94261b380d1aaa5b
marcello17 <marcello@gam.co.za>
2026-04-07 08:34:45 +0000

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fluss_api",
-    version="0.2.1",
+    version="0.2.2",
     packages=find_packages(include=["fluss_api", "fluss_api.*"]),
     install_requires=[
         "aiohttp>=3.7.0",
